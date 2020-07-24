a5565003d5476823b1e58fc47cc63bc15812bc8c
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-07-25 05:59:50 +0900

    preg_match

--- sample21.php
+++ sample21.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="css/style.css">
 
 <title>PHP</title>
 </head>
@@ -22,10 +22,13 @@
   $zip = '987-6543';
 
   $zip = mb_convert_kana($zip, 'a', 'UTF-8');
+  // マルチバイトコンバータカナで 変数を 半角英数字に変換し文字種設定
   if (preg_match("/\A\d{3}[-]\d{4}\z/", $zip)) {
-    print('郵便番号：〒'. $zip);
+    // preg_matchは指定の正規表現かどうか確認 第一、正規表現、第二、正規表現にしたい変数
+    print('郵便番号：〒' . $zip);
+    // 変数が正規表現と認識されたら、true
   } else {
-    print('郵便番号を 123-4567の形式で入力ください')；
+    print('郵便番号を 123-4567の形式で入力ください');
   }
   ?>
 </pre>
